09f96af42cee62691a6383c0c094ff70d94c101d
francesta <huntleyfrancesta@gmail.com>
2021-03-17 13:20:29 -0400

    git@github.com:huntleyfrancesta/phase-0-intro-to-js-2-array-methods.git

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+let cat0 = "Milo";
+let cat1 = "Otis";
+let cat2 = "Garfield";
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph")
+    console.log(cats) //=> ["Milo", "Otis", "Garfield", "Ralph"]
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift("Bob")
+    console.log(cats) //=> ["Bob", "Otis", "Garfield"]);
+}
+
+function destructivelyRemoveLastCat(Milo) {
+    cats.pop("Milo")
+    console.log(cats) //=> ["Otis", "Garfield"];
+}
+
+function destructivelyRemoveFirstCat(Otis) {
+    cats.shift("Otis")
+    console.log(cats) //=> ["Milo ,"Otis", "Garfield"]
+}
+
+function appendCat(name) {
+    return [...cats, 'Broom']
+};
+
+function prependCat(name) {
+    return ['Arnold', ...cats]
+};
+
+function removeLastCat(name) {
+    return [...cats.slice(0, 2)]
+};
+
+function removeFirstCat(name) {
+    return [...cats.slice(1)]
+};
